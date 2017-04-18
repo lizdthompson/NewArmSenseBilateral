6671f017f24f5c62396d678ca069bb4722edf767
Tyler Fettrow <tfettrow@gmail.com>
2017-04-18 15:49:50 -0400

    Added Start and Stop Button for accel collection

--- starter/src/main/java/com/mbientlab/metawear/tutorial/starter/MainActivity.java
+++ starter/src/main/java/com/mbientlab/metawear/tutorial/starter/MainActivity.java
@@ -13,6 +13,7 @@ import android.os.Bundle;
 import com.mbientlab.bletoolbox.scanner.BleScannerFragment;
 import com.mbientlab.metawear.MetaWearBoard;
 import com.mbientlab.metawear.android.BtleService;
+import com.mbientlab.metawear.module.Accelerometer;
 
 import java.util.UUID;
 
@@ -107,6 +108,10 @@ public class MainActivity extends AppCompatActivity implements BleScannerFragmen
     @Override
     public void onServiceConnected(ComponentName name, IBinder service) {
         serviceBinder = (BtleService.LocalBinder) service;
+        Accelerometer accelerometer = metawear.getModule(Accelerometer.class);
+        accelerometer.configure()
+                .odr(25f)       // Set sampling frequency to 25Hz, or closest valid ODR
+                .commit();
     }
 
     @Override
